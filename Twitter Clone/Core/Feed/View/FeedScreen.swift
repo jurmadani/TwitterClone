2ce02a4356d126caffcc771c71445f295ed85f93
Daniel Jurma <d_jurma@yahoo.com>
2023-11-24 12:39:43 +0200

    Added header for the feed screen

---
+++ Twitter Clone/Core/Feed/View/FeedScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FeedScreen.swift
+//  Twitter Clone
+//
+//  Created by Daniel Jurma on 24.11.2023.
+//
+
+import SwiftUI
+
+struct FeedScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedScreen()
+}
