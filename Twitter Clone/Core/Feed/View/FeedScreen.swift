122d3b12441968ab9f67795db2e2c0882f42fbb1
Daniel Jurma <d_jurma@yahoo.com>
2023-11-24 16:31:30 +0200

    Added feed screen header

--- Twitter Clone/Core/Feed/View/FeedScreen.swift
+++ Twitter Clone/Core/Feed/View/FeedScreen.swift
@@ -9,10 +9,42 @@ import SwiftUI
 
 struct FeedScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center) {
+            //header
+            Header()
+            Divider()
+            //feed posts
+            ScrollView{
+                Text("Feed postsss")
+            }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
     }
 }
 
+
 #Preview {
     FeedScreen()
+}
+
+struct Header: View {
+    var body: some View {
+        ZStack{
+            //avatar image
+            AsyncImage(url: URL(string: "https://i.pravatar.cc/32"))
+                .cornerRadius(99)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Spacer()
+            //twitter logo
+            TwitterLogo(frameWidth: 27, paddingTop: 0)
+            Spacer()
+            //icon
+            Image(systemName: "wand.and.stars")
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .foregroundColor(Color(UIColor(red: 29/255, green: 161/255, blue: 242/255, alpha: 1.0)))
+                .imageScale(.large)
+        }
+        .padding(.top, 10)
+        .padding(.leading, 10)
+        .padding(.trailing, 10)
+    }
 }
