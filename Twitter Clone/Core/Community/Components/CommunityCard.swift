cee3f3f380e564c9d51de6086e59d3beed519701
Daniel Jurma <daniel@gaim-solutions.com>
2023-12-22 19:30:06 +0200

    added communities tab view ui

---
+++ Twitter Clone/Core/Community/Components/CommunityCard.swift
@@ -0,0 +1,137 @@
+//
+//  CommunityCard.swift
+//  Twitter Clone
+//
+//  Created by Daniel Jurma on 22.12.2023.
+//
+
+import SwiftUI
+
+struct CommunityCard: View {
+    
+    var image: String
+    var title: String
+    var numberOfMembers: String
+    
+    var body: some View {
+        HStack{
+            // community logo
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 70)
+                .padding(.trailing)
+                .padding(.leading)
+            
+            VStack(alignment: .leading){
+                //title
+                Text(title)
+                    .fontWeight(.bold)
+                    .font(.headline)
+                //members
+                Text(numberOfMembers)
+                    .foregroundStyle(.secondary)
+                ZStack{
+                    AsyncImage(url: URL(string: "https://i.pravatar.cc/30")){ phase in
+                        if let image = phase.image {
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                .frame(width:32,height: 32)
+                                .cornerRadius(99)
+                            
+                        } else if phase.error != nil {
+                            Color.red // Indicates an error.
+                        } else {
+                            ProgressView()
+                        }
+                    }
+                    .zIndex(5)
+                  
+                    
+                    
+                    AsyncImage(url: URL(string: "https://i.pravatar.cc/30")){ phase in
+                        if let image = phase.image {
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                .frame(width:32,height: 32)
+                                .cornerRadius(99)
+                            
+                        } else if phase.error != nil {
+                            Color.red // Indicates an error.
+                        } else {
+                            ProgressView()
+                        }
+                    }
+                    .zIndex(4)
+                    .offset(x: 20, y: 0) // Adjust the x-offset to position it to the right of the first image
+                    
+                    AsyncImage(url: URL(string: "https://i.pravatar.cc/30")){ phase in
+                        if let image = phase.image {
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width:32,height: 32)
+                                .cornerRadius(99)
+                            
+                        } else if phase.error != nil {
+                            Color.red // Indicates an error.
+                        } else {
+                            ProgressView()
+                        }
+                    }
+                    .zIndex(3)
+                    .offset(x: 40, y: 0) // Adjust the x-offset to position it to the right of the first image
+                    
+
+                    AsyncImage(url: URL(string: "https://i.pravatar.cc/30")){ phase in
+                        if let image = phase.image {
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                .frame(width:32,height: 32)
+                                .cornerRadius(99)
+                            
+                        } else if phase.error != nil {
+                            Color.red // Indicates an error.
+                        } else {
+                            ProgressView()
+                        }
+                    }
+                    .zIndex(2)
+                    .offset(x: 60, y: 0) // Adjust the x-offset to position it to the right of the first image
+                    
+                    AsyncImage(url: URL(string: "https://i.pravatar.cc/30")){ phase in
+                        if let image = phase.image {
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                                .frame(width:32,height: 32)
+                                .cornerRadius(99)
+                            
+                        } else if phase.error != nil {
+                            Color.red // Indicates an error.
+                        } else {
+                            ProgressView()
+                        }
+                    }
+                    .zIndex(1)
+                    .offset(x: 80, y: 0) // Adjust the x-offset to position it to the right of the first image
+                    
+                }
+            }
+      
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        
+        Divider()
+            .padding(.top)
+    }
+        
+    
+}
+
+#Preview {
+    CommunityCard(image: "TwitterLogo", title: "Livestreaming on Twitter", numberOfMembers: "8k members")
+}
--- Twitter Clone/Core/Community/View/CommunityView.swift
+++ Twitter Clone/Core/Community/View/CommunityView.swift
@@ -16,10 +16,31 @@ struct CommunityView: View {
             //header
             ViewHeader(view: "community", showMenu: $showMenu)
             Divider()
-            //feed posts
+            //communities
             ScrollView{
-                Text("Community view")
+                CommunityCard(image: "TwitterLogo", title: "Livestreaming on Twitter", numberOfMembers: "12.4M members")
+                CommunityCard(image: "AiCommunityImage", title: "Join the new AI Community", numberOfMembers: "342.4M members")
+                CommunityCard(image: "GamingCommunityImage", title: "Gaming Communities", numberOfMembers: "153k members")
+                
+                //discover more
+                HStack{
+                    Button{
+                        
+                    } label: {
+                        Text("Show me more")
+                    }
+                    
+                    Spacer()
+                    
+                    Image(systemName: "chevron.right")
+                        .foregroundColor(.secondary)
+                }
+                .padding()
             }
+            
+            
+            
+            
         }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
     }
 }
